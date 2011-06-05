bbd31fdb0e822ea5342f9be42eb9e2a8f8b26166
Lee Braiden <lee.b@kintassa.com>
2011-06-05 13:54:28 +0100

    Display version numbers in about dialog, including development version handling

--- playaplayalib/conf.py
+++ playaplayalib/conf.py
@@ -1,15 +1,21 @@
 from ConfigParser import ConfigParser
 import os
 
-is_dev = os.path.exists(os.path.join(os.path.dirname(__file__), 'is_dev.txt'))
-
 class Conf(object):
     def __init__(self, app_name, *args, **kwargs):
         super(Conf, self).__init__(*args, **kwargs)
 
         self._conf_dir = os.path.expanduser("~/.config/%s" % app_name)
 
-        if is_dev:
+        from playaplayalib.git_tag import git_tag
+        if git_tag == "%d":
+            self._app_version = "(development version)"
+            self._is_dev = True
+        else:
+            self._app_version = git_tag
+            self._is_dev = False
+
+        if self._is_dev:
             self._conf_fpath = os.path.join(self._conf_dir, "%s_dev.conf" % app_name)
         else:
             self._conf_fpath = os.path.join(self._conf_dir, "%s.conf" % app_name)
@@ -29,6 +35,12 @@ class Conf(object):
     def __del__(self):
         self.save()
         super(Conf, self).__del__()
+
+    def get_version(self):
+        return self._app_version
+
+    def is_dev_version(self):
+        return self._is_dev
 
     def get_pos(self, playlist, default_fpath):
         playlist = os.path.abspath(playlist)
